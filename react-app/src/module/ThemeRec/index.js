5f985a2223295160bd04324321b4a0a0030e39eb
Kai-Chung <kaifresh@pchome.tw>
2022-06-30 18:21:00 +0800

    adjust prodinfo  with map

--- react-app/src/module/ThemeRec/index.js
+++ react-app/src/module/ThemeRec/index.js
@@ -5,6 +5,58 @@ import Pagination from '../../components/Pagination';
 import './themeRec.scss';
 
 const ThemeRec = () => {
+	const prodData = [
+		{
+			link: 'https://24h.pchome.com.tw/',
+			imageSrc: 'https://fakeimg.pl/300/',
+			imageAlt: 'ad',
+			title: 'Tokuyo 新nano玩美椅 按摩椅TC-263 (全機保固1年)',
+			price: '2156666',
+		},
+		{
+			link: 'https://24h.pchome.com.tw/',
+			imageSrc: 'https://fakeimg.pl/300/',
+			imageAlt: 'ad',
+			title: 'Tokuyo 新nano玩美椅 按摩椅TC-263 (全機保固1年)',
+			price: '2156666',
+		},
+		{
+			link: 'https://24h.pchome.com.tw/',
+			imageSrc: 'https://fakeimg.pl/300/',
+			imageAlt: 'ad',
+			title: 'Tokuyo 新nano玩美椅 按摩椅TC-263 (全機保固1年)',
+			price: '2156666',
+		},
+		{
+			link: 'https://24h.pchome.com.tw/',
+			imageSrc: 'https://fakeimg.pl/300/',
+			imageAlt: 'ad',
+			title: 'Tokuyo 新nano玩美椅 按摩椅TC-263 (全機保固1年)',
+			price: '2156666',
+		},
+		{
+			link: 'https://24h.pchome.com.tw/',
+			imageSrc: 'https://fakeimg.pl/300/',
+			imageAlt: 'ad',
+			title: 'Tokuyo 新nano玩美椅 按摩椅TC-263 (全機保固1年)',
+			price: '2156666',
+		},
+		{
+			link: 'https://24h.pchome.com.tw/',
+			imageSrc: 'https://fakeimg.pl/300/',
+			imageAlt: 'ad',
+			title: 'Tokuyo 新nano玩美椅 按摩椅TC-263 (全機保固1年)',
+			price: '2156666',
+		},
+	]
+	const renderProdItem = () => {
+    return prodData.map((objData) => (
+      <li className='c-themeRec__item'>
+        <ProdInfo {...objData} />
+      </li>
+    ));
+  };
+
 	return (
 		<div className="c-themeRec">
 			<div className="c-themeRec__adInfo">
@@ -13,9 +65,7 @@ const ThemeRec = () => {
 			</div>
 			<div className="c-themeRec__prodInfo">
 				<ul className='c-themeRec__list'>
-					<li className='c-themeRec__item'>
-						{<ProdInfo />}
-					</li>
+						{renderProdItem()}
 				</ul>
 				<Pagination item={3} />
 			</div>
