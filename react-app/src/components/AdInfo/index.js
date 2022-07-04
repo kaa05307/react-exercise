00d107c6cd225349a7aa2c589554cdff7ef05405
Kai-Chung <kaifresh@pchome.tw>
2022-07-04 11:22:35 +0800

    adinfo, pagination, prodinfo, themerec

--- react-app/src/components/AdInfo/index.js
+++ react-app/src/components/AdInfo/index.js
@@ -1,18 +1,42 @@
 import './adInfo.scss';
 
-const AdInfo = (props) => {
-  const { data } = props;
+const AdInfo = () => {
+  const keywordData = [
+    {
+			id: 1,
+      name: '#羅技滑鼠下殺9折'
+    },
+    {
+      id: 2,
+      name: '#電競筆電'
+    },
+    {
+      id: 3,
+      name: '#電競椅',
+    },
+    {
+      id: 4,
+      name: '#Acer主機+電競耳麥優惠組'
+    },
+    {
+      id: 5,
+      name: '#2020喇叭耳機推薦指南'
+    }
+  ]
+	const renderKeyword = () => {
+    return keywordData.map((objData) => (
+      <li key={objData.id}><a href="_"><span>{objData.name}</span></a></li>
+    ));
+  };
 
 	return (
     <div className="c-adInfo">
-      <span className="c-adInfo__tag">主題推薦</span>
+      <div className="c-adInfo__tag">
+        <span className='c-adInfo__text'>主題推薦</span>
+      </div>
       <h3 className='c-adInfo__title'>3C 電競年終戰</h3>
       <ul className='c-adInfo__keyword'>
-        <li><a href="_">#羅技滑鼠下殺9折</a></li>
-        <li><a href="_">#電競筆電</a></li>
-        <li><a href="_">#電競椅</a></li>
-        <li><a href="_">#Acer主機+電競耳麥優惠組</a></li>
-        <li><a href="_">#2020喇叭耳機推薦指南</a></li>
+        {renderKeyword()}
       </ul>
     </div>
 	);
--- react-app/src/components/Pagination/index.js
+++ react-app/src/components/Pagination/index.js
@@ -3,11 +3,11 @@ import './pagination.scss';
 const Pagination = ({item}) => {
 	return (
 		<ul className='c-pagination'>
-      <li className='c-pagination__item is-prev'>&lt;</li>
+      <li className='c-pagination__item is-prev'><i className='o-icon o-icon--prev'></i></li>
       <li className='c-pagination__item is-active'>1</li>
       <li className='c-pagination__item'>/</li>
       <li className='c-pagination__item is-total'>{item}</li>
-      <li className='c-pagination__item is-next'>&gt;</li>
+      <li className='c-pagination__item is-next'><i className='o-icon o-icon--next'></i></li>
     </ul>
 	);
 }
