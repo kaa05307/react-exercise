774e51e7c61f1643cde1028bbbd6551e8efdfb9b
Kai-Chung <kaifresh@pchome.tw>
2022-07-11 15:39:27 +0800

    adjust scss file

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './assets/scss/venders/_reset.scss';
+import './assets/scss/base/_base.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
