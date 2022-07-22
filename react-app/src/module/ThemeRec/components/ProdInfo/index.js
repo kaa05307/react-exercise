a00c39351e0c6ed002fe01dd6bbe2f893a660234
技術_大網_鄭凱中 <kaifresh@pchome.tw>
2022-07-22 10:59:09 +0800

    feat: adjust ProdInfo props

--- react-app/src/module/ThemeRec/components/ProdInfo/index.js
+++ react-app/src/module/ThemeRec/components/ProdInfo/index.js
@@ -1,8 +1,31 @@
+import React, { useState, useEffect } from 'react';
 import './prodInfo.scss';
 import ProdInfoItem from '../../../../components/Object/ProdInfoItem';
 import PropTypes from 'prop-types';
 
-const ProdInfo = ({ prodData }) => {
+const ProdInfo = ({ infoData }) => {
+  const [prodData, setProdData] = useState([]);
+
+	useEffect(() => {
+		const newData = infoData.map((item) => {
+			switch (item.ExtraData.ElementType) {
+				case 'Search':
+					item.Link.Url = `https://ecshweb.pchome.com.tw/search/v3.3/?q=${item.Link.Url}`;
+					break;
+				case 'Store':
+					item.Link.Url = `https://24h.pchome.com.tw/store/${item.Link.Url}`;
+					break;
+				case 'Prod':
+					item.Link.Url = `https://24h.pchome.com.tw/prod/${item.Link.Url}`;
+					break;
+				default:
+					break;
+			}
+			return item;
+		})
+		setProdData(newData)
+	}, [infoData]);
+
 	return (
     <ul className='c-prodInfo'>
       {
@@ -15,5 +38,12 @@ const ProdInfo = ({ prodData }) => {
     </ul>
 	);
 }
+
+ProdInfo.propTypes = {
+  infoData: PropTypes.array,
+};
+ProdInfo.defaultProps = {
+  infoData: [],
+};
 
 export default ProdInfo;
--- react-app/src/module/ThemeRec/index.js
+++ react-app/src/module/ThemeRec/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import AdInfo from './components/AdInfo';
 import AdBanner from './components/AdBanner';
 import ProdInfo from './components/ProdInfo';
@@ -8,28 +8,7 @@ import './themeRec.scss';
 
 const ThemeRec = () => {
 	const data = data1.window1.Blocks[0].Nodes;
-	const [prodData, setProdData] = useState([]);
 
-	useEffect(() => {
-		const newData = data.slice(6, 24).map((item) => {
-			switch (item.ExtraData.ElementType) {
-				case 'Search':
-					item.Link.Url = `https://ecshweb.pchome.com.tw/search/v3.3/?q=${item.Link.Url}`;
-					break;
-				case 'Store':
-					item.Link.Url = `https://24h.pchome.com.tw/store/${item.Link.Url}`;
-					break;
-				case 'Prod':
-					item.Link.Url = `https://24h.pchome.com.tw/prod/${item.Link.Url}`;
-					break;
-				default:
-					break;
-			}
-			return item;
-		})
-		setProdData(newData)
-	}, [data]);
-	
 	return (
 		<div className="c-themeRec">
 			<div className="c-themeRec__adInfo" style={{backgroundColor: data[0].Link.Background}}>
@@ -37,7 +16,7 @@ const ThemeRec = () => {
 				<AdBanner bannerData={data[0]} />
 			</div>
 			<div className="c-themeRec__prodInfo">
-				<ProdInfo prodData={prodData} />
+				<ProdInfo infoData={data.slice(6, 24)} />
 				<Pagination totalPage={(data.slice(6, 24).length)/6} />
 			</div>
 		</div>
