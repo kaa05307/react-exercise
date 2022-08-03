c2ed4db640acc0fc2722dff5ec0ade640cd45f53
技術_大網_鄭凱中 <kaifresh@pchome.tw>
2022-08-03 10:21:48 +0800

    feat: add axios & setupProxy.js

--- react-app/src/module/ThemeRec/index.js
+++ react-app/src/module/ThemeRec/index.js
@@ -1,18 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AdInfo from './components/AdInfo';
 import AdBanner from './components/AdBanner';
 import ProdInfo from './components/ProdInfo';
 import Pagination from '../../components/Pagination';
-import { data1 } from '../../data1';
+// import { data1 } from '../../data1';
+import _axios from 'axios';
 import './themeRec.scss';
 
 const ThemeRec = () => {
-	const data = data1.window1.Blocks[0].Nodes;
 	const [page, setPage] = useState(1);
+	const [data, setData] = useState([]);
+
+	useEffect(() => {
+		_axios.get('/index/stage/v1/data&27655702')
+			.then((res) => {
+				setData(res.data.window1.Blocks[0].Nodes);
+			})
+			.catch((err) => console.log(err))
+	}, []);
 
 	return (
 		<div className="c-themeRec">
-			<div className="c-themeRec__adInfo" style={{backgroundColor: data[0].Link.Background}}>
+			<div className="c-themeRec__adInfo" style={{backgroundColor: data[0]?.Link?.Background}}>
 				<AdInfo infoData={data[0]} keywordData={data.slice(1, 6)} />
 				<AdBanner bannerData={data[0]} />
 			</div>
