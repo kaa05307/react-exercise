b76f64fa382bc68b84891d36b8875ea47b4c5005
技術_大網_鄭凱中 <kaifresh@pchome.tw>
2022-08-19 12:03:25 +0800

    feat: add TodoList components

--- todolist-redux/src/App.js
+++ todolist-redux/src/App.js
@@ -1,35 +1,10 @@
-import { useState } from 'react';
+import React from 'react';
+import TodoList from './components/TodoList';
 import './App.css';
 
 function App() {
-  const [name, setName] = useState('');
-  const atChange = (e) => {
-    setName(e.target.value);
-  };
-  const atAddClick = () => {
-    console.log('add');
-  }
-  const atRemoveClick = () => {
-    console.log('remove');
-  }
-
   return (
-    <div className="todo">
-      <input type="text" placeholder='我是一個可愛的輸入框' value={name} onChange={atChange} />
-      <button onClick={atAddClick}>新增</button>
-      <table>
-        <tbody>
-          <tr>
-            <td>你好啊</td>
-            <td><button onClick={atRemoveClick}>刪除</button></td>
-          </tr>
-          <tr>
-            <td>我很好</td>
-            <td><button onClick={atRemoveClick}>刪除</button></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <TodoList />
   );
 }
 
---
+++ todolist-redux/src/components/TodoList/index.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const TodoList = () => {
+	const [name, setName] = useState('');
+  const atChange = (e) => {
+    setName(e.target.value);
+  };
+  const atAddClick = () => {
+    console.log('add');
+  }
+  const atRemoveClick = () => {
+    console.log('remove');
+  }
+
+	return (
+		<div className="todolist">
+      <input type="text" placeholder='我是一個可愛的輸入框' value={name} onChange={atChange} />
+      <button onClick={atAddClick}>新增</button>
+      <table>
+        <tbody>
+          <tr>
+            <td>你好啊</td>
+            <td><button onClick={atRemoveClick}>刪除</button></td>
+          </tr>
+          <tr>
+            <td>我很好</td>
+            <td><button onClick={atRemoveClick}>刪除</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+	);
+}
+ 
+export default TodoList;
