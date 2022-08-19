7ec5bb0f98e406daaa7c6db7eea6e6578e2c2658
技術_大網_鄭凱中 <kaifresh@pchome.tw>
2022-08-19 11:53:21 +0800

    feat: add button/input handler

--- todolist-redux/src/App.js
+++ todolist-redux/src/App.js
@@ -1,20 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('');
+  const atChange = (e) => {
+    setName(e.target.value);
+  };
+  const atAddClick = () => {
+    console.log('add');
+  }
+  const atRemoveClick = () => {
+    console.log('remove');
+  }
+
   return (
     <div className="todo">
-      <input type="text" placeholder='我是一個可愛的輸入框' />
-      <button>新增</button>
+      <input type="text" placeholder='我是一個可愛的輸入框' value={name} onChange={atChange} />
+      <button onClick={atAddClick}>新增</button>
       <table>
-        <tr>
-          <td>你好啊</td>
-          <td><button>刪除</button></td>
-        </tr>
-        <tr>
-          <td>我很好</td>
-          <td><button>刪除</button></td>
-        </tr>
+        <tbody>
+          <tr>
+            <td>你好啊</td>
+            <td><button onClick={atRemoveClick}>刪除</button></td>
+          </tr>
+          <tr>
+            <td>我很好</td>
+            <td><button onClick={atRemoveClick}>刪除</button></td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
