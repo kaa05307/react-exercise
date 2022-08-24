1d6e0c96501236e03e8b92cc266100bf53cbdd24
技術_大網_鄭凱中 <kaifresh@pchome.tw>
2022-08-24 10:37:45 +0800

    feat: install @reduxjs/toolkit package

--- todolist-redux/src/index.js
+++ todolist-redux/src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+// import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
+
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
 // const store = createStore(rootReducer);
+const store = configureStore({
+  reducer: {}
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <Provider store={store}>
+  <Provider store={store}>
     <App />
-  // </Provider>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
